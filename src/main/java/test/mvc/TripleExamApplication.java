abe033de87e9513f0f03f4724b20cb7ab5614434
jeonghyeon <wjdgsu53@naver.com>
2022-06-29 11:11:57 +0900

    dd

---
+++ src/main/java/test/mvc/TripleExamApplication.java
@@ -0,0 +1,13 @@
+package test.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripleExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripleExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/test/mvc/TripleExamApplicationTests.java
@@ -0,0 +1,13 @@
+package test.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripleExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
